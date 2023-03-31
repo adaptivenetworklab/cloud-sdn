6895cca381693c98ac5e0b118db1ae964b963dc7
MuhLitfan <muhlitfan88@gmail.com>
2023-03-31 13:16:33 +0700

    add script to create and delete manifest #2

--- k8s-deployment/Fat Tree/manifest/create-deployment.sh
+++ k8s-deployment/Fat Tree/manifest/create-deployment.sh
@@ -1,5 +1,5 @@
-kubectl apply -f ./flowvisor-comnetsemu-deployment.yaml
-kubectl apply -f ./flowvisor-comnetsemu-service.yaml
+kubectl apply -f ./flowvisor-deployment.yaml
+kubectl apply -f ./flowvisor-service.yaml
 kubectl apply -f ./center/
 kubectl apply -f ./left/
 kubectl apply -f ./right/
