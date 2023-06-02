bd0a1f88936c89647d456ee6f1266da655c09296
MuhLitfan <muhlitfan88@gmail.com>
2023-06-02 17:35:13 +0700

    stress test #26

--- scenario_testing/comnetsemu_1st/stress-test_monolith.sh
+++ scenario_testing/comnetsemu_1st/stress-test_monolith.sh
@@ -1,13 +1,13 @@
 sh sleep 10
 
 h4 ITGRecv -l /home/ubuntu/Hasil_Pengambilan_Data/Comnetsemu_1st/BGT/mono/recieve-h1-to-h4_100 &
-h1 ITGSend -T TCP -a 10.0.0.4 -c 100000 -C 1000 -t 80000 &
+h1 ITGSend -T TCP -a 10.0.0.4 -c 300000 -C 1000 -t 80000 &
 
 h5 ITGRecv -l /home/ubuntu/Hasil_Pengambilan_Data/Comnetsemu_1st/BGT/mono/recieve-h2-to-h5_100 &
-h2 ITGSend -T TCP -a 10.0.0.5 -c 100000 -C 1000 -t 80000 &
+h2 ITGSend -T TCP -a 10.0.0.5 -c 300000 -C 1000 -t 80000 &
 
 h6 ITGRecv -l /home/ubuntu/Hasil_Pengambilan_Data/Comnetsemu_1st/BGT/mono/recieve-h3-to-h6_100 &
-h3 ITGSend -T TCP -a 10.0.0.6 -c 100000 -C 1000 -t 80000 &
+h3 ITGSend -T TCP -a 10.0.0.6 -c 300000 -C 1000 -t 80000 &
 
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout="#rtp{dst=10.0.0.1, port=5004, mux=ts}" --ttl 12 --no-sout-all --sout-keep &
