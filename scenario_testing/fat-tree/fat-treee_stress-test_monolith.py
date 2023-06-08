fbec69869ed959e0936020ebb1b7d55e27bbe4ee
MuhLitfan <muhlitfan88@gmail.com>
2023-06-08 15:49:41 +0700

    bw readd

--- scenario_testing/fat-tree/fat-treee_stress-test_monolith.py
+++ scenario_testing/fat-tree/fat-treee_stress-test_monolith.py
@@ -15,9 +15,9 @@ class FVTopo(Topo):
         # Create template host, switch, and link
         hconfig = {"inNamespace": True}
         cpuconfig = {"cpu": .75/n}
-        #http_link_config = {"bw": 1}
-        #voip_link_config = {"bw": 0.5}
-        #video_link_config = {"bw": 3}
+        http_link_config = {"bw": 1}
+        voip_link_config = {"bw": 0.5}
+        video_link_config = {"bw": 3}
 
         # Create switch nodes
         for i in range(15):
@@ -31,55 +31,54 @@ class FVTopo(Topo):
             else:
                 self.addHost("h%d" % (i + 1), mac="00:00:00:00:00:%d" % (i + 1), **hconfig, **cpuconfig)
 
-        # Add switch links
-        self.addLink("s4", "s1")
-        self.addLink("s5", "s1")
+        self.addLink("s4", "s1", **video_link_config)
+        self.addLink("s5", "s1", **video_link_config)
         self.addLink("s6", "s1")
         self.addLink("s7", "s1")
         self.addLink("s8", "s1")
         self.addLink("s9", "s1")
-        self.addLink("s7", "s2")
+        self.addLink("s7", "s2", **http_link_config)
         self.addLink("s8", "s2")
         self.addLink("s9", "s2")
         self.addLink("s4", "s2")
         self.addLink("s5", "s2")
-        self.addLink("s6", "s2")
+        self.addLink("s6", "s2", **http_link_config)
         self.addLink("s4", "s3")
         self.addLink("s5", "s3")
         self.addLink("s6", "s3")
         self.addLink("s7", "s3")
-        self.addLink("s8", "s3")
-        self.addLink("s9", "s3")
-        self.addLink("s10", "s4")
-        self.addLink("s11", "s4")
-        self.addLink("s10", "s5")
-        self.addLink("s11", "s5")
+        self.addLink("s8", "s3", **voip_link_config)
+        self.addLink("s9", "s3", **voip_link_config)
+        self.addLink("s10", "s4", **video_link_config)
+        self.addLink("s11", "s4", **video_link_config)
+        self.addLink("s10", "s5", **video_link_config)
+        self.addLink("s11", "s5", **video_link_config)
         self.addLink("s12", "s5")
         self.addLink("s11", "s6")
-        self.addLink("s12", "s6")
-        self.addLink("s13", "s6")
-        self.addLink("s12", "s7")
-        self.addLink("s13", "s7")
+        self.addLink("s12", "s6", **http_link_config)
+        self.addLink("s13", "s6", **http_link_config)
+        self.addLink("s12", "s7", **http_link_config)
+        self.addLink("s13", "s7", **http_link_config)
         self.addLink("s14", "s7")
         self.addLink("s13", "s8")
-        self.addLink("s14", "s8")
-        self.addLink("s15", "s8")
-        self.addLink("s14", "s9")
-        self.addLink("s15", "s9")
+        self.addLink("s14", "s8", **voip_link_config)
+        self.addLink("s15", "s8", **voip_link_config)
+        self.addLink("s14", "s9", **voip_link_config)
+        self.addLink("s15", "s9", **voip_link_config)
 
         # Add host links
-        self.addLink("h1", "s10")
-        self.addLink("h2", "s10")
-        self.addLink("h3", "s11")
-        self.addLink("h4", "s11")
-        self.addLink("h5", "s12")
-        self.addLink("h6", "s12")
-        self.addLink("h7", "s13")
-        self.addLink("h8", "s13")
-        self.addLink("h9", "s14")
-        self.addLink("h10", "s14")
-        self.addLink("h11", "s15")
-        self.addLink("h12", "s15")
+        self.addLink("h1", "s10", **video_link_config)
+        self.addLink("h2", "s10", **video_link_config)
+        self.addLink("h3", "s11", **video_link_config)
+        self.addLink("h4", "s11", **video_link_config)
+        self.addLink("h5", "s12", **http_link_config)
+        self.addLink("h6", "s12", **http_link_config)
+        self.addLink("h7", "s13", **http_link_config)
+        self.addLink("h8", "s13", **http_link_config)
+        self.addLink("h9", "s14", **voip_link_config)
+        self.addLink("h10", "s14", **voip_link_config)
+        self.addLink("h11", "s15", **voip_link_config)
+        self.addLink("h12", "s15", **voip_link_config)
 
 
 topos = {"fvtopo": (lambda: FVTopo())}
