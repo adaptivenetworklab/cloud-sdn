2f673c46b38765d0429fa64f35c7300439b4b5d9
MuhLitfan <muhlitfan88@gmail.com>
2023-06-09 09:27:39 +0700

    stress test

--- scenario_testing/fat-tree/stress-test_monolith.sh
+++ scenario_testing/fat-tree/stress-test_monolith.sh
@@ -1,19 +1,19 @@
 sh sleep 5
 
-h4 iperf -s -p 5999 -b 15M &
-h2 iperf -c h4 -i 1 -t 40 -p 5999 -b 15M &
+h4 iperf -s -p 5999 -b 5 &
+h2 iperf -c h4 -i 1 -t 40 -p 5999 -b 5 &
 
-h8 iperf -s -p 5999 -b 15M &
-h6 iperf -c h8 -i 1 -t 40 -p 5999 -b 15M &
+h8 iperf -s -p 5999 -b 5 &
+h6 iperf -c h8 -i 1 -t 40 -p 5999 -b 5 &
 
-h12 iperf -s -p 5999 -b 15M &
-h10 iperf -c h12 -i 1 -t 40 -p 5999 -b 15M &
+h12 iperf -s -p 5999 -b 5 &
+h10 iperf -c h12 -i 1 -t 40 -p 5999 -b 5 &
 
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
 h7 python3 -m http.server 80 &
-h5 /root/pengujian_web-FatTree.sh &
+h5 /root/pengujian_web-FatTree.sh &5
 
 h11 ITGRecv -l recieve-h9-to-h11 &
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD &
