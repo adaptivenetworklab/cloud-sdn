0e3c99cb78efad7cc61959eecd276c67e5d25f35
MuhLitfan <muhlitfan88@gmail.com>
2023-06-09 09:23:41 +0700

    stress test

--- scenario_testing/fat-tree/stress-test_monolith.sh
+++ scenario_testing/fat-tree/stress-test_monolith.sh
@@ -1,13 +1,13 @@
 sh sleep 5
 
-h4 iperf -s -p 5999 -b 8M &
-h2 iperf -c h4 -i 1 -t 40 -p 5999 -b 8M &
+h4 iperf -s -p 5999 -b 15M &
+h2 iperf -c h4 -i 1 -t 40 -p 5999 -b 15M &
 
-h8 iperf -s -p 5999 -b 8M &
-h6 iperf -c h8 -i 1 -t 40 -p 5999 -b 8M &
+h8 iperf -s -p 5999 -b 15M &
+h6 iperf -c h8 -i 1 -t 40 -p 5999 -b 15M &
 
-h12 iperf -s -p 5999 -b 8M &
-h10 iperf -c h12 -i 1 -t 40 -p 5999 -b 8M &
+h12 iperf -s -p 5999 -b 15M &
+h10 iperf -c h12 -i 1 -t 40 -p 5999 -b 15M &
 
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
