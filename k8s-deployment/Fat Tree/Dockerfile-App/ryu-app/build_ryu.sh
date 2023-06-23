f7579f274f31ccc3d4f108aa396a4a45ee1529a0
MuhLitfan <muhlitfan88@gmail.com>
2023-06-23 14:44:26 +0700

    prepare manifest

--- k8s-deployment/Fat Tree/Dockerfile-App/ryu-app/build_ryu.sh
+++ k8s-deployment/Fat Tree/Dockerfile-App/ryu-app/build_ryu.sh
@@ -1,6 +1,6 @@
-docker image build -f middleware-left.Dockerfile -t adaptivenetlab/middleware-left:sw1.1 .
-docker image build -f middleware-center.Dockerfile -t adaptivenetlab/middleware-center:sw1.1 .
-docker image build -f middleware-right.Dockerfile -t adaptivenetlab/middleware-right:sw1.1 .
-docker image build -f ryu-app-left.Dockerfile -t adaptivenetlab/ryu-app-left:sw1.1 .
-docker image build -f ryu-app-center.Dockerfile -t adaptivenetlab/ryu-app-center:sw1.1 .
-docker image build -f ryu-app-right.Dockerfile -t adaptivenetlab/ryu-app-right:sw1.1 .
+docker image build -f middleware-left.Dockerfile -t adaptivenetlab/middleware-left:swf1.0 .
+docker image build -f middleware-center.Dockerfile -t adaptivenetlab/middleware-center:swf1.0 .
+docker image build -f middleware-right.Dockerfile -t adaptivenetlab/middleware-right:swf1.0 .
+docker image build -f ryu-app-left.Dockerfile -t adaptivenetlab/ryu-app-left:swf1.0 .
+docker image build -f ryu-app-center.Dockerfile -t adaptivenetlab/ryu-app-center:swf1.0 .
+docker image build -f ryu-app-right.Dockerfile -t adaptivenetlab/ryu-app-right:swf1.0 .
