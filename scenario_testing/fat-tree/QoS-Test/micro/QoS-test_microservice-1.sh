4d89aeb9f8d3e82fe368baa3f157fd5863814950
MuhLitfan <muhlitfan88@gmail.com>
2023-06-28 22:15:16 +0700

    new script bismillah

--- scenario_testing/fat-tree/QoS-Test/micro/QoS-test_microservice-1.sh
+++ scenario_testing/fat-tree/QoS-Test/micro/QoS-test_microservice-1.sh
@@ -1,9 +1,194 @@
 sh sleep 360
 
+
+
 h4 iperf -s -p 5999 -b 1M &
 h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 1M &
 
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-#sh tshark -i s4-eth5 -w /home/ubuntu/hasil_tshark-1 -F pcapng &
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/1 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+
+h4 iperf -s -p 5999 -b 2M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 2M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/2 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+h4 iperf -s -p 5999 -b 3M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 3M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/3 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+h4 iperf -s -p 5999 -b 4M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 4M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/4 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+h4 iperf -s -p 5999 -b 5M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 5M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/5 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+h4 iperf -s -p 5999 -b 6M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 6M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/6 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+h4 iperf -s -p 5999 -b 7M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 7M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/7 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+
+h4 iperf -s -p 5999 -b 8M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 8M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/8 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+h4 iperf -s -p 5999 -b 9M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 9M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/9 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
+
+
+
+
+h4 iperf -s -p 5999 -b 10M &
+h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 10M &
+
+h1 sudo -u ubuntu vlc rtp://@:5004 &
+h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
+
+sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/10 -F pcapng -a duration:60 &
+
+h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
+h2 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h4 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
+h1 sudo kill -9 $getjob
+h3 sudo kill -9 $getjob
+h2 sudo kill -9 $getjob
+h4 sudo kill -9 $getjob
