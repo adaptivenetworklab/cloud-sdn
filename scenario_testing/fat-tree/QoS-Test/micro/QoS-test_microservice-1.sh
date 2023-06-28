b23eee852246ad59d39f2ed4f8cb9e1427117900
MuhLitfan <muhlitfan88@gmail.com>
2023-06-28 23:04:06 +0700

    new script lagi

--- scenario_testing/fat-tree/QoS-Test/micro/QoS-test_microservice-1.sh
+++ scenario_testing/fat-tree/QoS-Test/micro/QoS-test_microservice-1.sh
@@ -8,7 +8,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 1M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/1 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/1 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -28,7 +28,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 2M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/2 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/2 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -47,7 +47,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 3M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/3 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/3 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -66,7 +66,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 4M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/4 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/4 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -85,7 +85,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 5M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/5 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/5 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -104,7 +104,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 6M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/6 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/6 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -123,7 +123,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 7M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/7 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/7 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -143,7 +143,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 8M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/8 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/8 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -162,7 +162,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 9M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/9 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/9 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
@@ -182,7 +182,7 @@ h2 iperf -c h4 -i 1 -t 18000 -p 5999 -b 10M &
 h1 sudo -u ubuntu vlc rtp://@:5004 &
 h3 sudo -u ubuntu vlc -q /home/ubuntu/cloud-sdn/scenario_testing/test_services/video.mp4 --sout='#rtp{dst=10.0.0.1, port=5004, mux=ts}' --ttl 12 --no-sout-all --sout-keep &
 
-sh tshark -i s4-eth5 -w /home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/hasil_test/10 -F pcapng -a duration:60 &
+sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/10 -F pcapng -a duration:60 &
 
 h1 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
 h3 getjob=$(ps aux | grep 'ubuntu vlc' | sed -n 1p | awk  '{print $2}')
