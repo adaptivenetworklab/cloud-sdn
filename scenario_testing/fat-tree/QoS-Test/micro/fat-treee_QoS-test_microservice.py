3f798ccadc10fe6da4ca53b055beaa1c9823d113
MuhLitfan <muhlitfan88@gmail.com>
2023-06-29 21:56:09 +0700

    new script

--- scenario_testing/fat-tree/QoS-Test/micro/fat-treee_QoS-test_microservice.py
+++ scenario_testing/fat-tree/QoS-Test/micro/fat-treee_QoS-test_microservice.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     # net.iperf( hosts = (h6, h8), l4Type='UDP', udpBw='1000M', seconds=100000, port = 5999 )
     # net.iperf( hosts = (h10, h12), l4Type='UDP', udpBw='1000M', seconds=100000, port = 5999 )
     
-    CLI.do_sh(net, 'sh sleep 360')
+    CLI.do_sh(net, 'sleep 360')
 
     # #Batch command execution for video streaming
     # video_test = "/home/ubuntu/cloud-sdn/scenario_testing/fat-tree/QoS-Test/micro/QoS-test_microservice-video.sh"
