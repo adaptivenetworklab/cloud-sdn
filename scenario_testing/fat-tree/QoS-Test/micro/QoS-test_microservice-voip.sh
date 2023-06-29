1265764c99497df59089df2cc1d74b31ba9b5e53
MuhLitfan <muhlitfan88@gmail.com>
2023-06-29 21:49:35 +0700

    new script

--- scenario_testing/fat-tree/QoS-Test/micro/QoS-test_microservice-voip.sh
+++ scenario_testing/fat-tree/QoS-Test/micro/QoS-test_microservice-voip.sh
@@ -6,119 +6,84 @@ h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
+
+
 
 
 h12 iperf -s -p 5999 -b 2M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 2M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/2 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
+
 
 
 
 h12 iperf -s -p 5999 -b 3M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 3M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/3 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
+
 
 
 
 h12 iperf -s -p 5999 -b 4M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 4M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/4 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
+
 
 
 
 h12 iperf -s -p 5999 -b 5M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 5M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/5 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
+
 
 
 
 h12 iperf -s -p 5999 -b 6M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 6M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/6 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
+
 
 
 
 h12 iperf -s -p 5999 -b 7M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 7M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/7 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
 
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
@@ -129,36 +94,23 @@ h12 sudo kill -9 $getjob
 h12 iperf -s -p 5999 -b 8M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 8M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/8 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
+
 
 
 
 h12 iperf -s -p 5999 -b 9M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 9M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/9 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
 
@@ -168,16 +120,9 @@ h12 sudo kill -9 $getjob
 h12 iperf -s -p 5999 -b 10M &
 h10 iperf -c h12 -i 1 -t 18000 -p 5999 -b 10M &
 
-
 h9 ITGSend -a 10.0.0.11 -rp 10003 VoIP -x G.711.2 -h RTP -VAD
-
-sh tshark -i s4-eth5 -w /root/hasil_test/micro/video/10 -F pcapng -a duration:60 &
-
-
 
 h10 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
 h12 getjob=$(ps | grep 'iperf' | awk  '{print $1}')
-
-
 h10 sudo kill -9 $getjob
 h12 sudo kill -9 $getjob
