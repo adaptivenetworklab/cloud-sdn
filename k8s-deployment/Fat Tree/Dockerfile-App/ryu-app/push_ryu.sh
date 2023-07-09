1ff7151e4cdece4a3b931759cdba6607d468e533
MuhLitfan <muhlitfan88@gmail.com>
2023-07-10 06:08:38 +0700

    versioning images

--- k8s-deployment/Fat Tree/Dockerfile-App/ryu-app/push_ryu.sh
+++ k8s-deployment/Fat Tree/Dockerfile-App/ryu-app/push_ryu.sh
@@ -1,6 +1,6 @@
-docker push adaptivenetlab/middleware-left:wf1.0
-docker push adaptivenetlab/middleware-center:wf1.0
-docker push adaptivenetlab/middleware-right:wf1.0
-docker push adaptivenetlab/ryu-app-left:wf1.0
-docker push adaptivenetlab/ryu-app-center:wf1.0
-docker push adaptivenetlab/ryu-app-right:wf1.0
+docker push adaptivenetlab/middleware-left:wf1.1
+docker push adaptivenetlab/middleware-center:wf1.1
+docker push adaptivenetlab/middleware-right:wf1.1
+docker push adaptivenetlab/ryu-app-left:wf1.1
+docker push adaptivenetlab/ryu-app-center:wf1.1
+docker push adaptivenetlab/ryu-app-right:wf1.1
